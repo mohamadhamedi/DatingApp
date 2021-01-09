a90194fa350d31fdcfecd8a21d7d97329266b121
MohamadHamedi <mohamadhamedi@yahoo.com>
2021-01-09 21:24:34 +0330

    Initial DataContext

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
